71c0ba5b28013108342991c51035c416425e38a3
Artem Fedorov <s02210577@gse.cs.msu.ru>
2023-12-06 12:14:15 +0300

    patches of ensambles.py module - adding bootstrap parameter to models - adding ability to choose splitter criterion

--- ensembles.py
+++ ensembles.py
@@ -1,12 +1,21 @@
+import warnings
 import numpy as np
 from scipy.optimize import minimize_scalar
 from sklearn.tree import DecisionTreeRegressor
+from pandas import DataFrame, Series
+
+
+def warn(*args, **kwargs):
+    pass
+
+
+warnings.warn = warn
 
 
 class RandomForestMSE:
     def __init__(
-        self, n_estimators, max_depth=None, feature_subsample_size=None,
-        random_state=42, **trees_parameters
+        self, n_estimators, *, max_depth=None, feature_subsample_size=None,
+        splitter='best', bootstrap=None, random_state=42, **trees_parameters
     ):
         """
         n_estimators : int
@@ -18,6 +27,12 @@ class RandomForestMSE:
         feature_subsample_size : float
             The size of feature set for each tree. If None then use one-third of all features.
 
+        splitter: ['best', 'random']
+            Criterion for splitting nodes in in trees
+
+        bootstrap: None | int | float
+            if None bootstraping is not performed. Otherwise defines splits for bootstraping
+
         random_state : int
             set the random state for estimators. 42 by default
         """
@@ -25,7 +40,9 @@ class RandomForestMSE:
 
         self.n_estimators = n_estimators
         self.max_depth = max_depth
+        self.splitter = splitter
         self.random_state = random_state
+        self.bootstrap = bootstrap
 
         if isinstance(feature_subsample_size, float) and \
                 (feature_subsample_size > 1.0 or feature_subsample_size < 0.0):
@@ -50,25 +67,40 @@ class RandomForestMSE:
             Array of size n_val_objects
         """
 
+        if isinstance(X, DataFrame):
+            X = X.to_numpy()
+        if isinstance(y, Series):
+            y = y.to_numpy()
+
         self.trees = []
+        rnd_gen = np.random.Generator(np.random.PCG64(self.random_state))
 
         if (isinstance(self.fss, int) and self.fss > X.shape[1]):
             raise ValueError(
                 'X have less features than expected by feature_subsample_size')
 
-        fss = self.fss if isinstance(
-            self.fss, int) else round(X.shape[1] * self.fss)
+        bootstrap = None
+        if self.bootstrap is not None:
+            if (isinstance(self.bootstrap, int) and self.bootstrap > X.shape[0] or
+                    isinstance(self.bootstrap, float) and (self.bootstrap > 1.0 or self.bootstrap < 0)):
+                raise ValueError('bootstrap index out of range')
+            else:
+                bootstrap = self.bootstrap if isinstance(
+                    self.bootstrap, int) else round(self.bootstrap * X.shape[0])
 
         for _ in range(self.n_estimators):
+            idx = np.random.permutation(
+                X.shape[0])[:bootstrap] if bootstrap is not None else np.arange(X.shape[0])
+
             tree = DecisionTreeRegressor(
                 criterion='squared_error',
-                splitter='random',
+                splitter=self.splitter,
                 max_depth=self.max_depth,
-                max_features=fss,
-                random_state=self.random_state,
+                max_features=self.fss,
+                random_state=rnd_gen.integers(0, 100_000_000),
                 **self.tree_parameters
             )
-            tree.fit(X, y)
+            tree.fit(X[idx], y[idx])
             self.trees.append(tree)
 
         return self
@@ -85,15 +117,16 @@ class RandomForestMSE:
         """
         if self.trees is None:
             raise ValueError('model is not fited')
+        preds = np.array([tree.predict(X) for tree in self.trees])
 
-        preds = [tree.predict(X).reshape((-1, 1)) for tree in self.trees]
-        return np.mean(np.array(preds), axis=1)
+        return np.mean(preds, axis=0)
 
 
 class GradientBoostingMSE:
     def __init__(
-        self, n_estimators, learning_rate=0.1, max_depth=5,
-        feature_subsample_size=None, random_state=42,
+        self, n_estimators, *, learning_rate=0.1, max_depth=5,
+        splitter='best', feature_subsample_size=None, random_state=42,
+        bootstrap=None,
         **trees_parameters
     ):
         """
@@ -109,6 +142,12 @@ class GradientBoostingMSE:
         feature_subsample_size : float | None
             The size of feature set for each tree. If None then use one-third of all features.
 
+        splitter: ['best', 'random']
+            Criterion for splitting nodes in in trees
+
+        bootstrap: None | int | float
+            if None bootstraping is not performed. Otherwise defines splits for bootstraping
+
         random_state : int
             set the random state for estimators. 42 by default
         """
@@ -118,6 +157,8 @@ class GradientBoostingMSE:
         self.lr = learning_rate
         self.max_depth = max_depth
         self.random_state = random_state
+        self.splitter = splitter
+        self.bootstrap = bootstrap
 
         if isinstance(feature_subsample_size, float) and \
                 (feature_subsample_size > 1.0 or feature_subsample_size < 0.0):
@@ -136,6 +177,11 @@ class GradientBoostingMSE:
         """
         self.trees = []
 
+        if isinstance(X, DataFrame):
+            X = X.to_numpy()
+        if isinstance(y, Series):
+            y = y.to_numpy()
+
         if (isinstance(self.fss, int) and self.fss > X.shape[1]):
             raise ValueError(
                 'X have less features than expected by feature_subsample_size')
@@ -143,18 +189,30 @@ class GradientBoostingMSE:
         fss = self.fss if isinstance(
             self.fss, int) else round(X.shape[1] * self.fss)
 
+        bootstrap = None
+        if self.bootstrap is not None:
+            if (isinstance(self.bootstrap, int) and self.bootstrap > X.shape[0] or
+                    isinstance(self.bootstrap, float) and (self.bootstrap > 1.0 or self.bootstrap < 0)):
+                raise ValueError('bootstrap index out of range')
+            else:
+                bootstrap = self.bootstrap if isinstance(
+                    self.bootstrap, int) else round(self.bootstrap * X.shape[0])
+
         grad = y
 
         for _ in range(self.n_estimators):
+            idx = np.random.permutation(
+                X.shape[0])[:bootstrap] if bootstrap is not None else np.arange(X.shape[0])
+
             tree = DecisionTreeRegressor(
                 criterion='squared_error',
-                splitter='best',
+                splitter=self.splitter,
                 max_features=fss,
                 random_state=self.random_state,
                 max_depth=self.max_depth,
                 **self.trees_parameters
             )
-            tree.fit(X, grad)
+            tree.fit(X[idx], grad[idx])
             self.trees.append(tree)
 
             grad = self.lr * (y - self.predict(X))
@@ -172,5 +230,5 @@ class GradientBoostingMSE:
         if self.trees is None:
             raise ValueError('model is not fited')
 
-        preds = [tree.predict(X).reshape((-1, 1)) for tree in self.trees]
-        return np.sum(np.array(preds), axis=1)
+        preds = np.array([tree.predict(X) for tree in self.trees])
+        return np.sum(preds, axis=0)
