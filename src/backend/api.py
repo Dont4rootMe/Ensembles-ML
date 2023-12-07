5bbb31cf545f26edf8aad152c7b4016ef86b2a9e
Artem Fedorov <s02210577@gse.cs.msu.ru>
2023-12-08 00:30:50 +0300

    initialize backend

---
+++ src/backend/api.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app = FastAPI(root_path=BACKEND_URL)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == '__main__':
+    uvicorn.run('api:app', host='0.0.0.0')
