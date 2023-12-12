898f873aee89dfe562607064c00ba4c6cf994738
Artem Fedorov <s02210577@gse.cs.msu.ru>
2023-12-12 15:53:16 +0300

    fixes of gui in front

--- src/frontend/src/UI/SynteticDataGenerator/SynteticDataGenerator.jsx
+++ src/frontend/src/UI/SynteticDataGenerator/SynteticDataGenerator.jsx
@@ -20,11 +20,13 @@ const SynteticDataGenerator = ({config, addHistory}) => {
 
 
     const trainModel = async (trace) => {
+
+        console.log(config)
         if (config.estimators.length <= 0 ) {
             addDanger('Число деревьев', 'Задайте явным образом параметр модели') 
             return
         }
-        if (config.learningRate.length <= 0 && config.model === 'grad-boosting') {
+        if (config.model === 'grad-boosting' && config.learningRate.length <= 0) {
             addDanger('Learning rate', 'Задайте явным образом learning rate') 
             return
         }
